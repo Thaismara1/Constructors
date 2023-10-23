c2165ba2b936b669f77c7dc6f9184bd651ec9783
Thaismara1 <thaismara91@gmail.com>
2023-10-23 15:43:45 +0100

    Add project files.

---
+++ Constructors/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();   
+        }
+        public Customer(int id)
+            : this()//e esse faz voce ir para o construtor vazio a cima
+            
+        {
+            this.Id = id;            
+        }
+
+        public Customer(int id, string name)
+            : this(id) //isso faz voce ir para o construtor id a cima
+                       //e assim ele vai rodar em todos para pegar todas as info.
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using Constructors;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var customer = new Customer();
+        customer.Id = 1;
+        customer.Name = "John";
+
+        Console.WriteLine(customer.Id);
+        Console.WriteLine(customer.Name);
+
+    }
+}
